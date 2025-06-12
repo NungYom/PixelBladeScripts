f03cacfe34f5c2efe99d59ef7441b6e656764472
NungYom <nungyom@gmail.com>
2025-06-12 15:39:43 +0700

    PBKA.lua

--- PBKA.lua
+++ PBKA.lua
@@ -10,7 +10,7 @@ local Humanoid = Character:WaitForChild("Humanoid")
 local HumanoidRootPart = Character:WaitForChild("HumanoidRootPart")
 
 -- Settings
-local moveSpeed = 100
+local moveSpeed = 50
 local orbitRadius = 5
 local autoMoveEnabled = true
 local touchedParts = {}

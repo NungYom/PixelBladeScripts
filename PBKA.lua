a1d95d6a7445b44f23f415a296696fc75edaad7c
NungYom <nungyom@gmail.com>
2025-07-24 18:33:36 +0700

    PBKA.lua

--- PBKA.lua
+++ PBKA.lua
@@ -1,38 +1,89 @@
-task.spawn(function()
-    while true do
-        local args1 = {{ UUID = "f77710bf-0754-4baa-ad8a-c18c9ad3f796" }}
-        game:GetService("ReplicatedStorage"):WaitForChild("REM"):WaitForChild("6fcf5d12-793d-4c91-a627-f6d0ffd973d8"):FireServer(unpack(args1))
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+local Workspace = game:GetService("Workspace")
+local Players = game:GetService("Players")
+local player = Players.LocalPlayer
+local function getEquippedToolUID()
+	local character = player.Character or player.CharacterAdded:Wait()
+	local tool = character:FindFirstChildOfClass("Tool")
+	if tool then
+		local uidValue = tool:FindFirstChild("UID")
+		if uidValue and typeof(uidValue.Value) == "string" then
+			return uidValue.Value
+		end
+	end
+	return nil
+end
+local itemId = 6
+local itemUID = getEquippedToolUID()
+if not itemUID then
+	warn("❌ ไม่พบปืนที่ถืออยู่หรือไม่มี UID")
+	return
+end
+local function fireAllRemote(uuid, fishUUID, direction, origin)
+	local args1 = {
+		"10000",
+		{ ItemId = itemId, UID = itemUID },
+		"1002",
+		uuid,
+		2
+	}
+	ReplicatedStorage.GameplayAbilitySystem.Remote.ServerActivateGameplayEffect:FireServer(unpack(args1))
 
-        local args2 = {{ UUID = "2e27ada9-0aa7-43ba-8015-2d222129ce81" }}
-        game:GetService("ReplicatedStorage"):WaitForChild("REM"):WaitForChild("6fcf5d12-793d-4c91-a627-f6d0ffd973d8"):FireServer(unpack(args2))
+	local args2 = {
+		"10000",
+		"1",
+		{ ItemId = itemId, UID = itemUID }
+	}
+	ReplicatedStorage.GameplayAbilitySystem.Remote.ClientApplyServerActivateGA:InvokeServer(unpack(args2))
 
-        local args3 = {{ Type = "Gold" }}
-        game:GetService("ReplicatedStorage"):WaitForChild("REM"):WaitForChild("ef87ca93-b2d5-43a0-9f8b-7cd17738c077"):FireServer(unpack(args3))
+	wait(0.05)
 
-        local args4 = {{ UUID = "fb9f5744-b6b2-48a0-b2fd-c22b9ec0a095" }}
-        game:GetService("ReplicatedStorage"):WaitForChild("REM"):WaitForChild("6fcf5d12-793d-4c91-a627-f6d0ffd973d8"):FireServer(unpack(args4))
+	local args3 = {
+		"10000",
+		tostring(itemId),
+		"12",
+		{
+			Direction = direction,
+			ThrowableId = itemId,
+			UUID = uuid,
+			Origin = origin
+		},
+		2,
+		"1002",
+		uuid
+	}
+	ReplicatedStorage.GameplayAbilitySystem.Remote.GameplayEffectExeFinish:FireServer(unpack(args3))
 
-        local args5 = {{ UUID = "fb93e67b-f7e7-4046-a0c2-6690ec2afc6f" }}
-        game:GetService("ReplicatedStorage"):WaitForChild("REM"):WaitForChild("6fcf5d12-793d-4c91-a627-f6d0ffd973d8"):FireServer(unpack(args5))
+	wait(0.05)
 
-        local args6 = {{ UUID = "10672693-233d-4bcd-ac56-227ec5a6875d" }}
-        game:GetService("ReplicatedStorage"):WaitForChild("REM"):WaitForChild("6fcf5d12-793d-4c91-a627-f6d0ffd973d8"):FireServer(unpack(args6))
+	local args4 = { "10000", "12", "1002", "1" }
+	ReplicatedStorage.GameplayAbilitySystem.Remote.ExeGameplayEffectStartFuncSucc:FireServer(unpack(args4))
 
-        local args7 = {{ UUID = "098fbba5-2fb7-4204-bcca-38b64fd2bd7d" }}
-        game:GetService("ReplicatedStorage"):WaitForChild("REM"):WaitForChild("6fcf5d12-793d-4c91-a627-f6d0ffd973d8"):FireServer(unpack(args7))
+	local args5 = { uuid }
+	ReplicatedStorage.Throwable.Remote.GetInstaceByThrowableIndex:InvokeServer(unpack(args5))
 
-        local args8 = {{ UUID = "fc6a0143-78d8-4af1-b2bc-69485512e18f" }}
-        game:GetService("ReplicatedStorage"):WaitForChild("REM"):WaitForChild("6fcf5d12-793d-4c91-a627-f6d0ffd973d8"):FireServer(unpack(args8))
+	wait(0.2)
 
-        local args9 = {{ UUID = "c7fe1b4b-1991-4525-815d-7091b86099ed" }}
-        game:GetService("ReplicatedStorage"):WaitForChild("REM"):WaitForChild("6fcf5d12-793d-4c91-a627-f6d0ffd973d8"):FireServer(unpack(args9))
+	local args6 = {
+		{
+			UUID = uuid,
+			FishList = { fishUUID }
+		}
+	}
+	ReplicatedStorage.Harpoon.Remote.ApplyCaptureResult:InvokeServer(unpack(args6))
+end
+while task.wait(0.5) do
+	pcall(function()
+		for _, fishFolder in pairs(Workspace:WaitForChild("FishModelList"):GetChildren()) do
+			local center = fishFolder:FindFirstChild("Center")
+			if center then
+				local uuid = fishFolder.Name
+				local direction = (center.Position - player.Character.HumanoidRootPart.Position).Unit
+				local origin = player.Character.HumanoidRootPart.Position
 
-        local args10 = {{ UUID = "084cd9ca-00c9-4e35-9d81-1f40cf93aaaf" }}
-        game:GetService("ReplicatedStorage"):WaitForChild("REM"):WaitForChild("6fcf5d12-793d-4c91-a627-f6d0ffd973d8"):FireServer(unpack(args10))
-
-        local args11 = {{ UUID = "c07fb6b3-0c01-4f50-9b6c-a980cab9800c" }}
-        game:GetService("ReplicatedStorage"):WaitForChild("REM"):WaitForChild("6fcf5d12-793d-4c91-a627-f6d0ffd973d8"):FireServer(unpack(args11))
-
-        task.wait(0.2)
-    end
-end)
+				fireAllRemote(uuid, uuid, direction, origin)
+				task.wait(0.2)
+			end
+		end
+	end)
+end
